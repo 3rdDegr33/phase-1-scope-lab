c817b1afb828dbacbe2247b9d26ffb069d75a5f7
Johnny Moya <profjmoya@gmail.com>
2021-10-15 17:25:39 -0500

    commit tree ae5da1e51df4059f43676a6585abf64fd8038aa5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = '';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newName) {
+    bestCustomer = newName;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'tom';
+}
